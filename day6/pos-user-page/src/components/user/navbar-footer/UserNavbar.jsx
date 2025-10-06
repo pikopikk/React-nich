7055eaf2176eac510db6bbb7f10e6f4289eff51c
AdulWazowski <taufikhan17@gmail.com>
2025-10-06 23:54:05 +0700

    day 6, kasih font style dari google fonts.

--- day6/pos-user-page/src/components/user/navbar-footer/UserNavbar.jsx
+++ day6/pos-user-page/src/components/user/navbar-footer/UserNavbar.jsx
@@ -1,6 +1,6 @@
 export default function UserNavbar(){
   return(
-    <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar bg-[#075B5E] shadow-sm">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
@@ -25,7 +25,7 @@ export default function UserNavbar(){
         <button className="btn btn-ghost btn-circle">
           <div className="indicator">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /> </svg>
-            <span className="badge badge-xs badge-primary indicator-item"></span>
+            <span className="badge badge-xs badge-primary indicator-item bg-[#FF3F33] border-0"></span>
           </div>
         </button>
       </div>
